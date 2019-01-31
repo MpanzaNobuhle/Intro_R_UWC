2543cacc188dc40fa35488394799a7ec7c0ed833
MpanzaNobuhle <mpanzanobuhle09@gmail.com>
2019-01-31 16:43:21 +0200

    Commit Homework Day3

---
+++ Day3_Homework.R
@@ -0,0 +1,29 @@
+#Day 3
+#NP Mpanza
+#30 January 2019
+#Homework
+
+#1.Practice how to read code for tomorrows class so you can read code in class and make it simple english.
+
+
+#2.Convert csv file to R formatted dataset with just one line of code:
+
+laminaria<- read.csv("laminaria.csv")
+save(laminaria, file = "laminaria.Rdata")
+
+
+#3. Describe and discuss ggsn and scale packages? library(ggsn) and library(scale).
+
+#a>ggsn: The ggsn package adds north symbols (18 options) and scale bars in kilometers to maps in geographic or metric coordinates created with 'ggplot' or 'ggmap'.
+
+
+#b>scale:This package is used to pull together palettes, bounding functions and transformations to provide a complete pathway from raw data to perceptual properties
+# scales package is made up Palettes, pal for short, describe the useful palettes of aesthetics.
+#Scales further has bounds that allows for various ways of rescaling the data.
+#Scales also provide methods for automatically determining breaks and labels for axes and legends.
+
+
+
+
+#Pushing
+# Go to git, then mark the files you want to push, then click commit, then on the box that pops up write your commit message. After that click commit,box pops close it then push.
